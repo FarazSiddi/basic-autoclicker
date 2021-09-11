4ddde8adce04b4ad388cea617e530b9447b6f67e
null <vr3c99@gmail.com>
2021-09-11 19:56:32 -0400

    Testing to see if Visual Studio is updating to GitHub properly.

--- AutoClicker/AutoClicker.cpp
+++ AutoClicker/AutoClicker.cpp
@@ -21,7 +21,7 @@ void clicker() {
             click = false;
         }
 
-        if (click == true) {
+        if (click == true) { // This if-statement block below runs when the autoclicker turns on
             mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
             mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
             Sleep(10); // Interval between each click
